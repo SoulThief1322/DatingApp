988ff85eb7a8d59dd0a1d18794ab83e92309878b
SoulThief1322 <alenko2007@gmail.com>
2024-12-08 21:38:14 +0200

    End of section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,10 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
   ]
 };
